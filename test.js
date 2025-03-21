baf49641e3b893e367c90b940b5d5beb044ab1df
Angel Maria Stanley <88926570+Maiden23@users.noreply.github.com>
2025-03-21 06:26:24 +0000

    Variables

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 console.log("Hello")
+
+const pi = 3.14  // constant value, you cant change value
+var a = 10
+let b = 15
+city = "Hyderabad"
+/*
+let is better to use as it's functional scope whereas var is not functional scope
+which can be an issue
+*/
+console.table([pi,a,b,city])
